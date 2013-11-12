37830e115290caf22c07e7f4ecbefe685f887423
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2013-11-12 15:27:50 +0100

    Fix typo: undefiend -> undefined

--- src/escalus_stanza.erl
+++ src/escalus_stanza.erl
@@ -266,7 +266,7 @@ remove_account() ->
 
 iq_result(Request) ->
     ToAttr = case exml_query:attr(Request, <<"from">>) of
-                 undefiend ->
+                 undefined ->
                      [];
                  Jid ->
                      [{<<"to">>, Jid}]
